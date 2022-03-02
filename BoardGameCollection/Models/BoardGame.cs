e3e142bac751e5aa0ff7a99d2e0cce4dbed9a1e7
Nick Sullivan <nicholassull@gmail.com>
2022-03-01 18:13:32 -0800

    update BoardGame.Id to BoardGameId

--- BoardGameCollection/Models/BoardGame.cs
+++ BoardGameCollection/Models/BoardGame.cs
@@ -2,7 +2,7 @@ namespace BoardGameCollection.Models
 {
   public class BoardGame
   {
-    public int Id { get; set;}
+    public int BoardGameId { get; set;}
     public string Name { get; set;}
     public int MaxPlayers { get; set;}
     public int Cooperative { get; set;}
